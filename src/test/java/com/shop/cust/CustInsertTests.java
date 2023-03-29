d54d968a2cdcf34e7456e2ee7ecc7a65fdf38b05
kyeong <kyeong4099@naver.com>
2023-03-29 17:00:04 +0900

    first

--- src/test/java/com/shop/cust/CustInsertTests.java
+++ src/test/java/com/shop/cust/CustInsertTests.java
@@ -9,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CustInsertTests {
 	@Autowired
-	CustService custService;
+	CustService custService;//
 
 	@Test
 	void contextLoads() {
